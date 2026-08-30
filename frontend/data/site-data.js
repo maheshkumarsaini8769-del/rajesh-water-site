29adde6c2000f0da2d60be975b53961085bd837b
maheshkumarsaini8769-del <maheshkumarsaini8769@gmail.com>
2026-08-30 16:00:26 +0530

    admin: update site-data

--- frontend/data/site-data.js
+++ frontend/data/site-data.js
@@ -213,7 +213,7 @@ window.SITE_DATA = {
       "price": 20,
       "boxSize": 12,
       "minBoxes": 1,
-      "img": "../images/bisleri.webp",
+      "img": "../images/products-0-img-1788085822293.webp",
       "badge": "Bestseller",
       "featured": true
     },
